85800afc56a2c01a538f088fc838bd0e0da333d3
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 02:33:23 +0500

    Adding the Picked Image to the Model and Add Place Form

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
+
 import 'package:facorite_places/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlaces(String title) {
-    final newPlace = Place(title: title);
+  void addPlaces(String title, File image) {
+    final newPlace = Place(title: title, image: image);
     state = [newPlace, ...state];
   }
 }
--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:facorite_places/providers/user_places.dart';
 import 'package:facorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +15,16 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
 
+  File? _selectedImage;
+
   void _savePlaces() {
     final enteredTitle = _titleController.text;
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
-    ref.read(userPlacesProvider.notifier).addPlaces(enteredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlaces(enteredTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -44,7 +50,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ),
             SizedBox(height: 10),
 
-            ImageInput(),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
             SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlaces,
