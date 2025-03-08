c288a641c2b92ba8414652d1208ef47fbfd5befe
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-09 02:29:29 +0500

    Adding a Location pkg and Starting with the get Location Input Widget

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).primaryColor.withValues(alpha: 0.5),
+            ),
+          ),
+          child: Text(
+            "No location chosen",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: Icon(Icons.location_on),
+              label: Text("Get Current Location"),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: Icon(Icons.map),
+              label: Text("Select on Map"),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
